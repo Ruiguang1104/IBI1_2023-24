882cee91d178430c7735c19576d7518a4e450183
Ruiguang1104 <156772064+Ruiguang1104@users.noreply.github.com>
2024-05-21 10:02:26 +0800

    Update get_duplicate_genes by crg 5/21.py

--- Practical8/get_duplicate_genes.py
+++ Practical8/get_duplicate_genes.py
@@ -1,10 +1,19 @@
 import re
-with open(r"C:\Users\86138\OneDrive - International Campus, Zhejiang University\桌面\ibi\IBI1_2023-24\IBI1_2023-24\Practical8\Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa",'r',encoding='utf-8') as file:
-    content=file.readlines()
-    for line in content:
-        if line.startswith('>') and 'duplication' in line:
-            match = re.search(r'gene:([A-Za-z0-9_-]+)\s', line)
-            if match:
-                print("the gene that we want:", match.group(1))
-                with open('duplicate_genes.fasta','a', encoding='utf-8') as new_file:
-                    new_file.write(match.group(1)+'\n')
+fasta_file_path = r'C:\Users\86138\OneDrive - International Campus, Zhejiang University\桌面\ibi ica\IBI1_2023-24\Practical8\Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa'
+#read the file
+genes_dict = {}
+#set a new empty dictionary
+with open(fasta_file_path, 'r') as fasta_file:
+    #open the file
+    for line in fasta_file:
+        if line.startswith('>'):#check the line that start with >
+            gene_name = line
+            genes_dict[gene_name] = ""
+        else:
+            genes_dict[gene_name] += line
+with open('duplicate_genes.fa','w') as f1:  
+        for gene_name, gene_sequence in genes_dict.items():
+            count = gene_name.count('duplication')
+            if count !=0:
+                simplified_name = str(re.findall(r'>(.+?)\s',gene_name))#try to find what we need
+                f1.write( simplified_name+'\n'+ gene_sequence + '\n')
