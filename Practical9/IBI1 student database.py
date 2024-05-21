e632c57f52bd3f7782ced3db5c8a82bccc47378b
Ruiguang1104 <156772064+Ruiguang1104@users.noreply.github.com>
2024-05-21 10:14:31 +0800

    Update IBI1 student database by crg 5/21.py

--- Practical9/IBI1 student database.py
+++ Practical9/IBI1 student database.py
@@ -1,9 +1,4 @@
-name1=input('please input student name:')
-major1=input('please input student major BMS or BMI:')
-self_homework_score1=input('please input student self homwork score:')
-group_project_score1=input('please input student group project score:')
-self_exam_score1=input('please input student self exam score:')
-#collect the student information
+
 class Student:
     def __init__(self,name,major,self_homework_score,group_project_score,self_exam_score):
         self.name = name
@@ -15,5 +10,14 @@ class Student:
     def print_student(self):
         print(f'student name:{self.name},major:{self.major},self_homework_score:{self.self_homework_score},group project score:{self.group_project_score}/100,self exam score:{self.self_exam_score}/100')
 #try to print it
+#an example
+student2=Student('CRG','BMS',90,85,90)
+student2.print_student()
+
+name1=input('please input student name:')
+major1=input('please input student major BMS or BMI:')
+self_homework_score1=input('please input student self homwork score:')
+group_project_score1=input('please input student group project score:')
+self_exam_score1=input('please input student self exam score:')#collect the student information
 student1=Student(name=name1,major=major1,self_homework_score=self_homework_score1,group_project_score=group_project_score1,self_exam_score=self_exam_score1)
 student1.print_student()
