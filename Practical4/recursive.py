1819fae2b03bad3a6dbc87d580f2d5a593d8c88e
Ruiguang1104 <156772064+Ruiguang1104@users.noreply.github.com>
2024-05-21 08:48:56 +0800

    Update recursive crg 5/21 commit.py

--- Practical4/recursive.py
+++ Practical4/recursive.py
@@ -1,6 +1,6 @@
-a=4
-#the first a+4
-print(a)
-for i in range(1,5):
+a=4#first number
+i=1#set i as the calculator
+while i <= 5:
+    print(a)
     a=2*a+3# calculaste the next members
-    print(a)#then print the next 4 members in the sequence
+    i +=1 
